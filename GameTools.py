cb7f59e9d0a78ab63a7831eb0b709f09cb77f5ea
albloh2 <albloh2@gmail.com>
2022-05-28 18:37:21 +0800

    Working Shop System

--- GameTools.py
+++ GameTools.py
@@ -1,20 +1,36 @@
+import math as m
+
+import pygame
 import pygame as pg
+
+PYGAME_API = pg
 
 # PYGAME COLORS #
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+# PYGAME CURSOR #
+ARROW = pg.SYSTEM_CURSOR_ARROW
+HAND = pg.SYSTEM_CURSOR_HAND
+WAIT = pg.SYSTEM_CURSOR_WAIT
 
-# PYGAME TOOLS #
+
+# PYGAME WINDOWS #
 class Window:
-    def __init__(self, width, height):
-        self.window = pg.display.set_mode((width, height))
+    def __init__(self, width, height, scaled=False):
+        if scaled:
+            self.window = pg.display.set_mode((width, height), pg.SCALED | pg.FULLSCREEN)
+        else:
+            self.window = pg.display.set_mode((width, height))
         self.clock = pg.time.Clock()
         self.fps = 60
-        self.centered = False
 
     def set_fps(self, fps):
         self.fps = fps
+
+    @staticmethod
+    def set_icon(icon):
+        pg.display.set_icon(icon)
 
     @staticmethod
     def set_title(title):
@@ -23,14 +39,204 @@ class Window:
     def fill_color(self, colour):
         self.window.fill(colour)
 
-    def draw_text(self, text_object, text, colour, x, y, centered=False):
-        text_object.draw(self, text, colour, x, y, centered)
+    def draw_image(self, image_object, x, y):
+        image_object.draw(self, x, y)
+
+    def draw_text(self, text_object, text, colour, x, y):
+        text_object.draw(self, text, colour, x, y)
+
+    def draw_sprite(self, sprite):
+        sprite.draw(self)
+
+    def draw_rectangle(self, rectangle_object, colour, x, y, filled=True, outline=1):
+        rectangle_object.draw(self, colour, x, y, filled, outline)
+
+    def draw_button(self, button_object, color, text):
+        button_object.draw(self, color=color, text=text)
 
     def update(self):
         pg.display.update()
         self.clock.tick(self.fps)
 
+    def run(self, process, draw):
+        run = True
+        while run:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    run = False
+            if run:
+                run = process(self.clock.get_time() / 1000 * 30)
+                draw(self)
+                self.update()
 
+
+# PYGAME INPUT #
+class Mouse:
+    def __init__(self):
+        self.pressed = pg.mouse.get_pressed()
+        self.position = pg.mouse.get_pos()
+        self.movement = pg.mouse.get_rel()
+        self.visible = pg.mouse.get_visible()
+        self.focused = pg.mouse.get_focused()
+        self.cursor = pg.mouse.get_cursor()
+
+    def get_pressed(self):
+        self.pressed = pg.mouse.get_pressed()
+        return self.pressed
+
+    def get_position(self):
+        self.position = pg.mouse.get_pos()
+        return self.position
+
+    def set_position(self, x, y):
+        pg.mouse.set_pos(x, y)
+        self.position = pg.mouse.get_pos()
+
+    def get_movement(self):
+        self.movement = pg.mouse.get_rel()
+        return self.movement
+
+    def get_visible(self):
+        self.visible = pg.mouse.get_visible()
+        return self.visible
+
+    def set_visible(self, visible):
+        pg.mouse.set_visible(visible)
+        self.visible = pg.mouse.get_visible()
+
+    def get_focused(self):
+        self.focused = pg.mouse.get_focused()
+        return self.focused
+
+    def get_cursor(self):
+        self.cursor = pg.mouse.get_cursor()
+        return self.cursor
+
+    def set_cursor(self, cursor):
+        pg.mouse.set_cursor(cursor)
+        self.cursor = pg.mouse.get_cursor()
+
+    def tick(self):
+        self.pressed = pg.mouse.get_pressed()
+        self.position = pg.mouse.get_pos()
+        self.movement = pg.mouse.get_rel()
+        self.visible = pg.mouse.get_visible()
+        self.focused = pg.mouse.get_focused()
+        self.cursor = pg.mouse.get_cursor()
+
+
+class Keyboard:
+    def __init__(self):
+        self.pressed = pg.key.get_pressed()
+        self.modifiers = pg.key.get_mods()
+        self.visible = pg.key.get_focused()
+
+    def get_pressed(self):
+        self.pressed = pg.key.get_pressed()
+        return self.pressed
+
+    def get_modifiers(self):
+        self.modifiers = pg.key.get_mods()
+        return self.modifiers
+
+    def get_visible(self):
+        self.visible = pg.key.get_focused()
+        return self.visible
+
+    def tick(self):
+        self.pressed = pg.key.get_pressed()
+        self.modifiers = pg.key.get_mods()
+        self.visible = pg.key.get_focused()
+
+
+# PYGAME IMAGE #
+
+class Image:
+    def __init__(self, args, mode="f"):
+        width, height = pg.display.get_surface().get_size()
+        self.HALF_WIDTH = round(width / 2)
+        self.HALF_HEIGHT = round(height / 2)
+        if mode == "f":
+            self.image = pg.image.load(args)
+        elif mode == "o":
+            self.image = args
+        else:
+            raise ValueError(f"Invalid Option: '{mode}'")
+
+    def rotate(self, angle):
+        self.image = pg.transform.rotate(self.image, -angle)
+
+    def resize(self, width, height, smooth=True):
+        if smooth:
+            self.image = pg.transform.smoothscale(self.image, (width, height))
+        else:
+            self.image = pg.transform.scale(self.image, (width, height))
+
+    def scale(self, scale, smooth=True):
+        if smooth:
+            self.image = pg.transform.smoothscale(self.image, (
+                round(self.image.get_width() * scale), round(self.image.get_height() * scale)))
+        else:
+            self.image = pg.transform.scale(self.image, (
+                round(self.image.get_width() * scale), round(self.image.get_height() * scale)))
+
+    def draw(self, window, x, y):
+        window.window.blit(self.image, (x + self.HALF_WIDTH - (self.image.get_width() / 2),
+                                        self.HALF_HEIGHT - y - (self.image.get_height() / 2)))
+
+
+class Sprite:
+    def __init__(self, image, x, y, mode="f"):
+        self.smooth = True
+        width, height = pg.display.get_surface().get_size()
+        self.HALF_WIDTH = round(width / 2)
+        self.HALF_HEIGHT = round(height / 2)
+        if mode == "f":
+            self.image = Image(image, mode="f")
+        elif mode == "o":
+            self.image = image
+        else:
+            raise ValueError(f"Invalid Option: '{mode}'")
+        self.x = x
+        self.y = y
+        self.size = 100
+        self.rotation = 0
+
+    def get_image(self):
+        return self.image
+
+    def get_surface(self):
+        return self.image.image
+
+    def clone(self):
+        return Sprite(Image(self.get_surface().copy(), "o"), self.x, self.y, "o")
+
+    def rotate(self, angle):
+        self.rotation = angle
+        self.image.rotate(angle)
+
+    def resize(self, size):
+        self.image.scale(size / self.size, self.smooth)
+        self.size = size
+
+    def move(self, steps):
+        self.x += m.sin(m.radians(self.rotation)) * steps
+        self.y += m.cos(m.radians(self.rotation)) * steps
+
+    def clicked(self, mouse_object):
+        if mouse_object.get_pressed()[0]:
+            hit_box = pygame.Rect(self.x + self.HALF_WIDTH - (self.image.image.get_width() / 2),
+                                  self.HALF_HEIGHT - self.y - (self.image.image.get_height() / 2),
+                                  self.image.image.get_width(), self.image.image.get_height())
+            if hit_box.collidepoint(mouse_object.get_position()):
+                return True
+        return False
+
+    def draw(self, window):
+        self.image.draw(window, self.x, self.y)
+
+
+# PYGAME TEXT #
 class Text:
     def __init__(self, font, font_size):
         width, height = pg.display.get_surface().get_size()
@@ -38,18 +244,82 @@ class Text:
         self.HALF_WIDTH = round(width / 2)
         self.HALF_HEIGHT = round(height / 2)
 
-    def draw(self, window, text, colour, x, y, centred=False):
+    def get_image(self, text, colour):
+        return Image(self.Font.render(text, True, colour), "o")
+
+    def draw(self, window, text, colour, x, y):
         text_to_render = self.Font.render(text, True, colour)
-        if window.centered and centred:
-            window.window.blit(text_to_render, (x + self.HALF_WIDTH - (text_to_render.get_width() / 2),
-                                                self.HALF_HEIGHT - y - (text_to_render.get_height() / 2)))
-        elif window.centered:
-            window.window.blit(text_to_render, (x + self.HALF_WIDTH, self.HALF_HEIGHT - y))
-        elif centred:
-            window.window.blit(text_to_render, (x + (text_to_render.get_width() / 2),
-                                                y - (text_to_render.get_height() / 2)))
+        window.window.blit(text_to_render, (x + self.HALF_WIDTH - (text_to_render.get_width() / 2),
+                                            self.HALF_HEIGHT - y - (text_to_render.get_height() / 2)))
+
+
+class SystemText:
+    def __init__(self, font, font_size):
+        width, height = pg.display.get_surface().get_size()
+        self.Font = pg.font.SysFont(font, font_size)
+        self.HALF_WIDTH = round(width / 2)
+        self.HALF_HEIGHT = round(height / 2)
+
+    def get_image(self, text, colour):
+        return Image(self.Font.render(text, True, colour), "o")
+
+    def draw(self, window, text, colour, x, y):
+        text_to_render = self.Font.render(text, True, colour)
+        window.window.blit(text_to_render, (x + self.HALF_WIDTH - (text_to_render.get_width() / 2),
+                                            self.HALF_HEIGHT - y - (text_to_render.get_height() / 2)))
+
+
+# PYGAME SHAPES #
+
+class Rectangle:
+    def __init__(self, width, height):
+        w_width, w_height = pg.display.get_surface().get_size()
+        self.width = width
+        self.height = height
+        self.HALF_WIDTH = round(w_width / 2)
+        self.HALF_HEIGHT = round(w_height / 2)
+
+    def get_rect(self):
+        return pg.Rect(0, 0, self.width, self.height)
+
+    def draw(self, window, colour, x, y, filled=True, outline=1):
+        if filled:
+            pg.draw.rect(window.window, colour,
+                         pygame.Rect(x + self.HALF_WIDTH - (self.width / 2), self.HALF_HEIGHT - y - (self.height / 2),
+                                     self.width, self.height))
         else:
-            window.window.blit(text_to_render, (x, y))
+            pg.draw.rect(window.window, colour,
+                         pygame.Rect(x + self.HALF_WIDTH - (self.width / 2), self.HALF_HEIGHT - y - (self.height / 2),
+                                     self.width, self.height), outline)
+
+
+# PYGAME GUI #
+
+class Button(Sprite):
+    def __init__(self, image, text_object, x, y):
+        super().__init__(image, x, y)
+        self.text_object = text_object
+
+    def draw(self, window, **kwargs):
+        super().draw(window)
+        self.text_object.draw(window, kwargs["text"], kwargs["color"], self.x, self.y)
 
 
 pg.init()
+
+if __name__ == "__main__":
+    def __process(_delta_time):
+        return True
+
+
+    def __update(windows):
+        windows.fill_color(WHITE)
+        windows.draw_text(rootFont, "About PyGameTools", BLACK, 0, 20)
+        windows.draw_text(rootFont, "PyGameTools V1.0 (pygame 2.1.2)", BLACK, 0, 0)
+        windows.draw_text(rootFont, "The Font Should Be Comic Sans", BLACK, 0, -20)
+
+
+    root = Window(800, 600)
+    root.set_title("PyGameTools")
+    rootFont = SystemText("Comic Sans MS", 20)
+    root.run(__process, __update)
--- main.py
+++ main.py
@@ -1,30 +1,128 @@
-import pygame as pg
+import os
 import GameTools as gt
 import constants as const
-import os
+
+
+def process(_delta_time):
+    global Mode
+    if Mode == "main_menu":
+        if Mouse.get_pressed()[0]:
+            print(Mouse.get_position())
+            load_level(1)
+            Mode = "game_1"
+    elif Mode == "game_1":
+        if ButtonPlay.clicked(Mouse):
+            return False
+        handle_ui_buy_shooter()
+
+    return True
 
 
 def update(windows):
-    windows.fill_color(gt.WHITE)
-    windows.draw_text(Font, "Hello World!", gt.BLACK, 0, 0, True)
-    windows.update()
+    if Mode == "main_menu":
+        windows.fill_color(gt.WHITE)
+        windows.draw_text(Font48, "Tower Defense Game", gt.BLACK, 0, -50)
+        windows.draw_text(Font48, "Click Anywhere To Start", gt.BLACK, 0, -100)
+        windows.draw_sprite(ShooterSprite)
+    elif Mode == "game_1":
+        windows.draw_image(LevelBackground, 0, 0)
+        windows.draw_rectangle(UIBarMain, gt.BLACK, 0, -335)
+        windows.draw_text(Font24, f"Wave: {Wave}", gt.WHITE, -590, -335)
+        windows.draw_text(Font24, f"Health: {Health}", gt.WHITE, -450, -335)
+        windows.draw_text(Font24, f"Money: ${Money}", gt.WHITE, -300, -335)
+        windows.draw_button(ButtonPlay, gt.BLACK, "Start")
+        windows.draw_sprite(ShooterSprite)
+        windows.draw_text(Font24, f"${const.SHOOTER_PRICE}", gt.WHITE, -150, -335)
+        for i in range(len(ShooterSpriteClones)):
+            windows.draw_sprite(ShooterSpriteClones[i])
+        # windows.draw_button(ButtonShop, gt.BLACK, "Shop")
+        windows.draw_text(Font24, "Alpha Preview. Version 1.0.0-a.1.", gt.BLACK, -495, 345)
+        return
+    elif Mode == "loser":
+        windows.fill_color(gt.WHITE)
+        windows.draw_text(Font48, "You Loser", gt.BLACK, 0, 0)
+    windows.draw_text(Font24, "Alpha Preview. Version 1.0.0-a.1.", gt.BLACK, -495, -345)
+
+
+def handle_ui_buy_shooter():
+    global IsClicked, Money
+    if ShooterSprite.clicked(Mouse) and not IsClicked and Money >= const.SHOOTER_PRICE:
+        IsClicked = True
+        Money -= const.SHOOTER_PRICE
+        ShooterSpriteClones.append(ShooterSprite.clone())
+    if IsClicked:
+        ShooterSpriteClones[len(ShooterSpriteClones) - 1].x = \
+            Mouse.get_position()[0] - gt.PYGAME_API.display.get_surface().get_size()[0] / 2
+        ShooterSpriteClones[len(ShooterSpriteClones) - 1].y = \
+            gt.PYGAME_API.display.get_surface().get_size()[1] / 2 - Mouse.get_position()[1]
+    if not Mouse.get_pressed()[0] and IsClicked:
+        IsClicked = False
+        if ShooterSpriteClones[len(ShooterSpriteClones) - 1].y < -300:
+            ShooterSpriteClones.pop()
+            Money += const.SHOOTER_PRICE
+
+
+def splash_screen():
+    global Mode
+    Mode = "splash"
+    Window.fill_color(gt.WHITE)
+    Window.draw_text(Font24, "Albloh2 Gaming", gt.BLACK, 0, 0)
+    Window.draw_text(Font24, "Alpha Preview. Version 1.0.0-a.1.", gt.BLACK, -495, -345)
+    Window.update()
+    Mouse.set_cursor(gt.WAIT)
+    gt.PYGAME_API.time.wait(1000)
+    Mouse.set_cursor(gt.ARROW)
+    Mode = "main_menu"
+
+
+def load_assets():
+    global Font24, Font48, ShooterSprite
+    Font24 = gt.Text(os.path.join("Assets", "TD_Toon Around.otf"), 24)
+    Font48 = gt.Text(os.path.join("Assets", "TD_Toon Around.otf"), 48)
+    ShooterSprite = gt.Sprite(os.path.join("Assets", "TD_Shooter_1.svg"), 0, 50)
+
+
+def load_level(level_id):
+    global LevelBackground, UIBarMain, Wave, ButtonPlay, ButtonShop, Money, Health
+    Mouse.set_cursor(gt.WAIT)
+    Window.fill_color(gt.BLACK)
+    Window.draw_text(Font24, "Loading...", gt.WHITE, 0, 0)
+    Window.draw_text(Font24, "Alpha Preview. Version 1.0.0-a.1.", gt.WHITE, -495, -345)
+    Window.update()
+    gt.PYGAME_API.time.wait(1000)
+    LevelBackground = gt.Image(os.path.join("Assets", f"TD_Level_{level_id}.png"))
+    UIBarMain = gt.Rectangle(1280, 50)
+    ButtonPlay = gt.Button(os.path.join("Assets", "TD_Button_1.svg"), Font24, 590, -335)
+    ShooterSprite.resize(100)
+    ShooterSprite.x = -200
+    ShooterSprite.y = -335
+    # ButtonShop = gt.Button(os.path.join("Assets", "TD_Button_1.svg"), Font24, 500, -335)
+    Wave = 0
+    Money = const.INITIAL_MONEY
+    Health = const.INITIAL_HEALTH
+    Mouse.set_cursor(gt.ARROW)
 
 
 def main():
-    global Font
-    window = gt.Window(const.WIDTH, const.HEIGHT)
-    window.set_title("Pygame Game")
-    Font = gt.Text(os.path.join("Assets", "Toon Around.otf"), 24)
-    window.centered = True
-    frame_rate = pg.time.Clock()
-    run = True
-    while run:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = False
-        update(window)
-        frame_rate.tick(const.FPS)
-    pg.quit()
+    global Font24, Font48, ShooterSprite, Mouse, Keyboard, Mode, LevelBackground, UIBarMain, Wave, Window, IsClicked, \
+        ShooterSpriteClones
+    LevelBackground = None
+    UIBarMain = None
+    Wave = None
+    IsClicked = False
+    ShooterSpriteClones = []
+    Mode = "init"
+    Window = gt.Window(const.WIDTH, const.HEIGHT)
+    Window.set_title("Tower Defense Game Alpha Preview")
+    load_assets()
+    ShooterSprite.rotate(90)
+    Window.set_icon(ShooterSprite.get_surface())
+    ShooterSprite.resize(200)
+    Mouse = gt.Mouse()
+    Keyboard = gt.Keyboard()
+    splash_screen()
+    Window.run(process, update)
+
 
 if __name__ == "__main__":
     main()
